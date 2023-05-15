2e51f48e6a982927a2180f10fd4d875e4a94d2dd
siddiquirizwan6969@gmail.com <mdehteshamshaikh1@gmail.com>
2023-05-15 19:41:59 +0530

    cors

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.get('/working', (req, res) => {
     console.log('working');
     return res.status(200).json({
